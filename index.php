639df8069a628e5f44986cbc33c129324fdf95cb
EC2 Default User <l>
2020-05-02 18:18:03 +0900

    test

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <?php get_header(); //header.phpを取得 ?>
     
     <main>
-        <section>
+        <section class=section1>
         <div class="main_photo"><!--ここには写真が入る-->
         </div>
-        <?php
-        ?>
+        
         <div class="main_container">
             
             <div class="about_container">
                 <div class="about_background"></div>
-            
-        
-                <div class="about_page">
+                    <div class="about_page">
                     <div class=myphoto_img><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/myphoto.png"></img>
                     <div class="thenkyou_text">
                         <h3>お忙しい中、僕のホームページに来てくださりありがとうございます。
@@ -46,8 +43,46 @@
               </div><!--about_container-->
             </div><!--main_container-->
             </section>
+            <div class="works_container">
+            
+            <div class="child_container">
+                <div class="child_background"></div>
+                    <div class="child_page">
+                    <div class="thenkyou_text">
+                        <h3>僕が作成した作品です。模写作品が多くなっています。今後徐々に増やしていきます。
+                        <br>どんなスキルでも貪欲に学んでいきたいと思っています。
+                        <br>模写作品もありますが少しでも見て頂けると幸いです。フィードバックを頂けると感激です。
+                        </h3>
+                        </div><!--thenkyou_text-->
+                    <h2>ABOUT ME</h2>
+                    <div class="explanatory_text">
+                    <P>出身：愛知県瀬戸市瀬戸物焼きが有名な場所。</p>
+                    <br>
+                    <p>学歴：工業高校情報化卒業<br>　　　京都外国語専門学校卒業</P>
+                    <br>
+                    <p>職歴：YUMEX株式会社 入社<br>　　　部署内で年間営業成績２５名中３位を獲得</P>
+                    <br>
+                    <p>　　：Culture Convenience Club 入社</p>
+                    <p>　　　スターバックス事業部に配属。８年間で８店舗を経験。
+                    現在店長としてお仕事しています。（２０２０年 5月）</P>
+                    <br>
+                    <p>夢 :  高校生の頃の夢はエンジニアでした。何を思ったのか外国語学校に進学。
+                    新卒でIT企業に内定をもらうも辞退。エンジニアは敷居が高くて自分には
+                    無理と思い込んでいた。しかし夢諦めきれず31歳からプログラミング学習
+                    開始。人に喜んでもらうのが好きなのでアプリを通じて少しでも幸せになれるようなものを作りたい。</P>
+                    <br>
+                    <p>趣味特技：カメラ・動画編集・ランニング・服・お酒
+		            インドにて象乗りの資格を習得（乗れば取れます）</p>
+		            </div>
+                 </div><!--myphoto_img-->
+                    
+                </div><!--about_page-->
+              </div><!--about_container-->
+            </div><!--main_container-->
+            </section>
             <section>
             <div class="works_container">
+                <div class="child_container">
                 <div class="child_works">
                     <h2>WORKS</h2>
                     <div class="box1">
@@ -72,6 +107,7 @@
                         </p>
                     </div>
                  </div><!--child_works-->
+                </div><!--child_container-->
              </div><!--works_container-->
             </section>
             
