2c86d4aa865b8430fa96adb96e21aa4db1342acb
EC2 Default User <blowinblues582@gmail.com>
2020-05-23 02:56:04 +0900

    first test

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
       <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); //ピングバックURLを出力 ?>">
       <!-- Bootstrap CSS -->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-      <?php wp_head(); //wp_headはテーマの</head>タグ直前に必ず挿入します ?>
+      
       <script>
   (function(d) {
     var config = {
@@ -18,6 +18,7 @@
     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
   })(document);
 </script>
+<?php wp_head(); //wp_headはテーマの</head>タグ直前に必ず挿入します ?>
   </head>
   <body>
      <header>
